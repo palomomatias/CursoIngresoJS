5ce8c7ee79bd7e1049008eaf062720364d86f0dc
palomomatias <palomomatias@hotmail.com>
2018-02-05 12:33:46 -0300

    me quede en ejercicio 5

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,10 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostar()
 {
-	
+    console.log("asd")
+    console.info("dsa")
+    console.error("asdsad")
+    alert("hola")
+  
 }
 
--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -4,6 +4,8 @@
 */
 function Mostar()
 {
-	
+	var nombre;
+	nombre = prompt("pone tu nombre","");
+	document.getElementById("elNombre").value = nombre
 }
 
